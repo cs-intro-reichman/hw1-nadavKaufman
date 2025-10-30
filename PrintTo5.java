fa1a58b62cb4fc793756f2f2b90c1148b87959d1
Nadav Kaufman <nadav1252@gmail.com>
2025-10-30 12:09:01 +0200

    done

--- PrintTo5.java
+++ PrintTo5.java
@@ -4,6 +4,9 @@ public class PrintTo5 {
 	public static void main(String[] args) {
 		// Declares an int variable, and sets it to 1
         int i = 0;
+        int a=1;
+        //prints the variable
+        System.out.println(a);
         // Complete the program's code here:
     }
  }
